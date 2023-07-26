88070ceac8ce1f35acd793b020aebe3813a6bb38
Ameer Ghazy <ameerghazy@Ameers-MacBook-Pro.local>
2023-07-26 17:29:42 -0400

    adding band_name_generator.py

---
+++ 100_python_projects_challenge/beginner/band_name_generator.py
@@ -0,0 +1,8 @@
+#1. Welcome message
+print ("Welcome to the Band Name Generator.")
+#2. Ask the user for the city that they grew up in.
+city = input ("What's name of the city you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input ("What's name of your pet?\n")
+#4. Combine name of city and pet to suggest band name.
+print ( "Your band name could be " + city + " "+ pet_name )
